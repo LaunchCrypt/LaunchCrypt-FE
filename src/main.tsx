f8e03ac9d9cf9809efd5a249bac0c12100dc49f6
ctb0k33 <ctb0k33@gmail.com>
2024-10-07 18:27:19 +0700

    set-up-router

---
+++ src/main.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { router } from "./router/router";
+import { RouterProvider } from "react-router-dom";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
