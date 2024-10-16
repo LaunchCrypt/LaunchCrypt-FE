a74ebbbf91c28c0cda1207a9e90c09c9ee7aeabe
ctb0k33 <ctb0k33@gmail.com>
2024-10-16 20:31:03 +0700

    set-up socket

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -7,6 +7,23 @@ import PairBanner from '../components/PairBanner/PairBanner'
 import TokenPriceHistoryChart from '../components/chart/TokenPriceHistoryChart'
 
 import '../App.css'
+
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:3000');
+
+socket.on('connect', () => {
+  console.log('Connected to WebSocket server');
+});
+
+socket.on('btcPrice', (data) => {
+  console.log('BTC Price:', data.price, 'Time:', new Date(data.time));
+});
+
+socket.on('disconnect', () => {
+  console.log('Disconnected from WebSocket server');
+});
+
 function MainPage() {
   return (
     <div className='flex flex-col align-middle w-full h-full items-center'>
