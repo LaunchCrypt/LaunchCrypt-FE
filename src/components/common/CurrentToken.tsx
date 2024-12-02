61cae2969ee730257bccc9cb4f262bb876a31e3d
ctb0k33 <ctb0k33@gmail.com>
2024-12-02 10:47:09 +0700

    chore [change token image address]

--- src/components/common/CurrentToken.tsx
+++ src/components/common/CurrentToken.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Inetwork } from '../../interfaces'
+import avaxLogo from "../../../assets/icons/Avalanche-logo.svg"
 
 function CurrentToken(network:{network: Inetwork}) {
     return (
         <div className='flex flex-row items-center h-8 rounded-[100px] p-[8px_12px_8px_8px] gap-1 cursor-pointer bg-[#24243a] hover:bg-[#3e3e52]'>
-            <img src={`../../../assets/icons/${network.network.image}`} className='w-6 h-6'/>
+            <img src={avaxLogo} className='w-6 h-6'/>
             <span className='text-base font-medium'>{network.network.symbol}</span>
         </div>
     )
